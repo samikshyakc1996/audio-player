07030e500e2af3d69cc29dc0988d54767312373d
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-12-17 10:52:04 -0500

    structured with comments

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-const playOrPause = document.querySelector(`#playOrPause`);
-const playprev = document.querySelector(`#playprev`);
-const playnext = document.querySelector(`#playnext`);
-const playlistEle = document.querySelector(`#playlists`);
-const playlist = [
+//UTILITY FUNCTIONS
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+// DOCUMENT ELEMENTS
+const playOrPause = $(`#playOrPause`);
+const playprev = $(`#playprev`);
+const playnext = $(`#playnext`);
+const playlistEle = $(`#playlists`);
+
+//VARIABLES
+let playlistIndex = 0;
+const song = new Audio();
+//DATA
+export const playlist = [
   `./audio/10 Adele - Lovesong.mp3`,
   `/Users/samikshyakc/Desktop/audio-player/audio/All You Need Is Love - Remastered 2009.mp3`,
   `/Users/samikshyakc/Desktop/audio-player/audio/Ticket To Ride - Remastered 2009.mp3`,
   `/Users/samikshyakc/Desktop/audio-player/audio/With A Little Help From My Friends - Remastered 2009.mp3`,
 ];
-let playlistIndex = 0;
-const song = new Audio();
-song.src = playlist[playlistIndex];
 
-playlist.forEach(function (item, index) {
-  playlistEle.innerHTML += `<li data-index="${index}">${item}</li>`;
+window.addEventListener(`load`, function () {
+  //SETUP THE FIRST SONG TO PLAY
+  song.src = playlist[playlistIndex];
+
+  //LOAD UP THE PLAYLIST
+  playlist.forEach(function (item, index) {
+    playlistEle.innerHTML += `<li data-index="${index}">${item}</li>`;
+  });
 });
 playlistEle.addEventListener(`click`, function (event) {
   const songToPlay = event.target;
@@ -24,17 +37,19 @@ playlistEle.addEventListener(`click`, function (event) {
     song.play();
     playOrPause.textContent = `⏸`;
   }
-});
-playOrPause.addEventListener(`click`, function (event) {
-  if (song.paused) {
-    song.play();
-    playOrPause.textContent = `⏸`;
-  } else {
-    song.pause();
-    playOrPause.textContent = `▶️`;
-  }
+  // IF PLAY/PAUSE IS CLICKED
+  playOrPause.addEventListener(`click`, function (event) {
+    if (song.paused) {
+      song.play();
+      playOrPause.textContent = `⏸`;
+    } else {
+      song.pause();
+      playOrPause.textContent = `▶️`;
+    }
+  });
 });
 
+// WHEN NEXT SONG BUTTON IS CLICKED
 playnext.addEventListener(`click`, function (event) {
   console.log(`next btn clicked`);
   playOrPause.textContent = `⏸`;
@@ -45,6 +60,7 @@ playnext.addEventListener(`click`, function (event) {
   song.src = playlist[playlistIndex];
   song.play(`${playlist[playlistIndex]}`);
 });
+// WHEN PREVIOUS SONG BUTTON IS CLICKED
 playprev.addEventListener(`click`, function (event) {
   playOrPause.textContent = `⏸`;
   console.log(`prev btn clicked`);
